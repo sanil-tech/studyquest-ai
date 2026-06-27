c3ac8a617e6346bd789e9d6832d506cbcea2fbf0
SANIYIL BANSAI - <sanil@ums.edu.my>
2026-06-27 17:30:08 +0000

    Manual code change

--- base44/functions/createChildAccount/entry.ts
+++ base44/functions/createChildAccount/entry.ts
@@ -1,30 +1,10 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
 
 // ===============================
-// UTILITIES (CLEAN + SAFE)
+// UTILITIES
 // ===============================
 
 const generateStudentId = () => {
   const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
   let id = 'SQ-';
-  for (let i = 0; i < 6; i++) {
-    id += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return id;
-};
-
-const generatePassword = () => {
-  const upper = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
-  const lower = 'abcdefghijkmnopqrstuvwxyz';
-  const numbers = '23456789';
-  const special = '@#$%';
-
-  let password = '';
-  password += upper[Math.floor(Math.random() * upper.length)];
-  password += lower[Math.floor(Math.random() * lower.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
-  password += special[Math.floor(Math.random() * special.length)];
-
-  const all = upper + lower + numbers + special;
-  for (let i = 0; i < 6; i++) {
-    password += all[Math.floor(Math.random() * all.length)];
+  for (let i = 0; i <
